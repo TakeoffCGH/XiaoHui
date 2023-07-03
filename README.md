b28123eefa706311c087d1e7ab9a3bcc270bf0d6
PiGe <qi_fei18813792227@qq.com>
2023-07-04 02:27:59 +0800

    upload project

---
+++ README.md
@@ -0,0 +1,11 @@
+# 云小慧
+
+后端
+
+大学校园课表小程序实战课程开源项目
+
+UI设计稿地址：[https://js.design/f/LlnLBx?p=7PluEepcpc](https://js.design/f/LlnLBx?p=7PluEepcpc)
+
+> **测试号为 stuId: test，password: 123456**
+
+
---
+++ pages/login/index.js
@@ -0,0 +1,77 @@
+import {
+  loginRequest
+} from "../../api/main"
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    stuId: '', // 学号
+    password: '', // 密码
+    saveCount: true, // 是否记住账号，默认选中
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad(options) {
+    this.initAccount()
+  },
+
+  // 初始化账号
+  initAccount() {
+    const accountCache = wx.getStorageSync("account")
+    if (accountCache) {
+      this.setData({
+        ...accountCache
+      })
+    }
+  },
+
+  // 登录
+  login() {
+    const that = this
+    const postData = {
+      stuId: that.data.stuId,
+      password: that.data.password
+    }
+    wx.showLoading({
+      title: '登录中',
+    })
+    loginRequest(postData).then(res => {
+      wx.hideLoading()
+      if (res.code == -1) {
+        wx.showToast({
+          title: res.msg,
+          icon: 'none'
+        })
+        return
+      }
+      if (that.data.saveCount) {
+        wx.setStorageSync('account', postData)
+      }
+      wx.setStorageSync('token', res.data.cookie)
+      if (that.data.saveCount) {
+        wx.setStorageSync('account', postData)
+      } else {
+        wx.removeStorageSync('account')
+      }
+      wx.showToast({
+        title: '登录成功',
+        icon: 'none'
+      })
+      setTimeout(() => {
+        wx.switchTab({
+          url: '/pages/index/index',
+        })
+      }, 1500);
+    })
+  },
+
+  switchStatus() {
+    this.setData({
+      saveCount: !this.data.saveCount
+    })
+  }
+})
